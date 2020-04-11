e4e4a780c9fb739f58849d655396a515b2ff04da
baraschner <aschner5@gmail.com>
2020-04-11 13:57:00 -0700

    removed accidental copy

--- brain/cli/api_resources/base_resource.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-
-from brain.utils import consts
-
-
-class BaseResource(Resource):
-    def __init__(self, db_connection):
-        self.db_connection = db_connection[consts.DB_NAME]
