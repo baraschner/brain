0c6771c54f0887c8bfac88ef397cb0908b56421e
baraschner <aschner5@gmail.com>
2020-04-11 10:26:04 -0700

    tests

--- brain/server/server.py
+++ brain/server/server.py
@@ -1,5 +1,5 @@
 import json
-
+import logging
 from flask import Flask, request
 from flask_restful import Resource, Api
 from bson import BSON
@@ -12,7 +12,10 @@ from brain.parsers import Parser
 
 def run(host, port, url):
     app = Flask(__name__)
+    logger = logging.getLogger()
+    app.logger = logger
     api = Api(app)
+
 
     queue = build_queue_connection_from_url(url)
     queue.exchange_declare(consts.PARSER_INPUT_EXCHANGE_NAME, 'fanout')
