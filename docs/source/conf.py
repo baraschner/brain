d426abec2f47ebac124f3bd027c0d750a2ed2033
baraschner <aschner5@gmail.com>
2020-04-12 01:22:48 -0700

    changed theme style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('../../'))
 
 
@@ -41,7 +42,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx-bootstrap-theme'
+html_theme = 'bootstrap'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
