7da37f24e00268cee97d2c41293d8b1bc24da285
baraschner <aschner5@gmail.com>
2020-04-12 12:30:06 -0700

    server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,21 +1,23 @@
-from pytest import fixture
-
 import json
-import requests
 import multiprocessing
 import time
 from pathlib import Path
 
-from brain.server import run_server
+import requests
+from pytest import fixture
+
 from brain.client import upload_sample
-from brain.utils import consts
+from brain.server import run_server
+
 _SAMPLE_PATH = Path('tests/data/sample.mind.gz').absolute()
+_SERVER_IP = '127.0.0.1'
+_SERVER_TEST_PORT = 4080
 
 
 @fixture()
 def server_fixture(publish):
-    func,path = publish
-    process = multiprocessing.Process(target=run_server, args=('127.0.0.1', 4080, None, func,))
+    func, path = publish
+    process = multiprocessing.Process(target=run_server, args=(_SERVER_IP, _SERVER_TEST_PORT, None, func,))
     process.start()
     time.sleep(2)
     try:
@@ -26,20 +28,20 @@ def server_fixture(publish):
 
 @fixture()
 def publish(tmp_path):
-
     def on_message(m):
-        with open(tmp_path/'result','w') as f:
+        with open(tmp_path / 'result', 'w') as f:
             f.write(m)
-    yield on_message,tmp_path
+
+    yield on_message, tmp_path
 
 
 def test_config(server_fixture):
-    result = json.loads(requests.get('http://127.0.0.1:4080/config').json())
+    result = json.loads(requests.get(f'http://{_SERVER_IP}:{_SERVER_TEST_PORT}/config').json())
     assert 'feelings' in result['supported_fields']
 
 
 def test_snapshot(server_fixture):
-    upload_sample(_SAMPLE_PATH, '127.0.0.1', 4080)
-    with open(server_fixture/'result') as f:
+    upload_sample(_SAMPLE_PATH, _SERVER_IP, _SERVER_TEST_PORT)
+    with open(server_fixture / 'result') as f:
         d = json.load(f)
     assert 'userId' in d and 'feelings' in d
