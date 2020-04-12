28af57df220266b409aab1b6626076ae18f964ed
baraschner <aschner5@gmail.com>
2020-04-12 02:09:55 -0700

    change to material

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,8 @@ author = 'Bar Aschner'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc']
+extensions.append('sphinx_material')
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
