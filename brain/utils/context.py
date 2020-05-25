da72a8f45562a91e2b62594b481a931982374db5
Bar Aschner <aschner5@gmail.com>
2020-05-25 22:28:21 +0300

    Refactor utils

--- brain/utils/context.py
+++ brain/utils/context.py
@@ -2,12 +2,10 @@ from pathlib import Path
 
 
 class Context:
-
     def __init__(self, base_path, user_id, datetime):
         self.base_path = Path(base_path)
         self.user_id = user_id
         self.datetime = datetime
-
         self.base_path.mkdir(parents=True, exist_ok=True)
 
     def path(self, filename):
--- brain/utils/string_utils.py
+++
@@ -1,2 +0,0 @@
-def datetime_string_format(datetime_str):
-    return datetime_str.replace(" ", "_").replace(":", "-")
