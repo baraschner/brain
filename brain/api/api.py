c4c852fa3e5f701d7e2aacae58fe30664413b30a
baraschner <aschner5@gmail.com>
2020-05-29 08:31:56 -0700

    change api tests to use flask framework

--- brain/api/api.py
+++ brain/api/api.py
@@ -6,10 +6,11 @@ from pymongo import MongoClient
 from brain.utils import flask_tools, consts
 
 
-def run_api_server(host, port, database_url=None, db=None):
+def run_api_server(host, port, database_url=None, db=None, test=False):
     """
     Runs the api server.
 
+    :param test: testing configuration in which the app is returned and not run.
     :param host: ip to bind
     :param port: port to bind
     :param database_url: url of database from which the api will get the data
@@ -24,5 +25,7 @@ def run_api_server(host, port, database_url=None, db=None):
 
     # automatically initializes api from api_resources
     flask_tools.api_driver(Api(app), __file__, (db,))
+    if test:
+        return app
 
     app.run(host=host, port=port)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,8 @@
-import multiprocessing
-import time
 from pathlib import Path
 
 from pytest import fixture
 
 from brain.api import run_api_server
-from brain.cli import *
 
 _SERVER_HOST = "127.0.0.1"
 _SERVER_PORT = 5000
@@ -14,49 +11,34 @@ _USERID = 42
 _SNAPSHOT_ID = "5ed10b5776f0aa9affa9e3f0"
 
 
-@fixture(autouse=True)
-def api_fixture(mongodb):
-    process = multiprocessing.Process(target=run_api_server,
-                                      args=(_SERVER_HOST, _SERVER_PORT, None, mongodb,))
-    process.start()
-    time.sleep(2)
-    try:
-        yield
-    finally:
-        process.kill()
-
-
 @fixture()
-def binary_data():
-    with open(_DEPTHIMAGE_PATH, 'rb') as f:
-        return f.read()
+def api_fixture(mongodb):
+    app = run_api_server(_SERVER_HOST, _SERVER_PORT, db=mongodb, test=True)
+    return app.test_client()
 
 
-def test_get_users():
-    result = requests.get(f'http://{_SERVER_HOST}:{_SERVER_PORT}/users').json()
+def test_get_users(api_fixture):
+    result = api_fixture.get(f'/users').json
     assert result == [{'username': 'Dan Gittik', 'userId': 42}]
 
 
-def test_get_user():
-    result = requests.get(f'http://{_SERVER_HOST}:{_SERVER_PORT}/users/{_USERID}').json()
+def test_get_user(api_fixture):
+    result = api_fixture.get(f'/users/{_USERID}').json
     assert result == {'userId': 42, 'birthday': 699746400, 'gender': 'MALE', 'username': 'Dan Gittik'}
 
 
-def test_get_snapshots():
-    result = requests.get(f'http://{_SERVER_HOST}:{_SERVER_PORT}/users/{_USERID}/snapshots').json()
+def test_get_snapshots(api_fixture):
+    result = api_fixture.get(f'/users/{_USERID}/snapshots').json
     assert result == [{'datetime': 1575446887339, 'id': '5ed10b5776f0aa9affa9e3f0'}]
 
 
-def test_get_snapshot():
-    result = requests.get(f'http://{_SERVER_HOST}:{_SERVER_PORT}/users/{_USERID}/snapshots/{_SNAPSHOT_ID}').json()
+def test_get_snapshot(api_fixture):
+    result = api_fixture.get(f'/users/{_USERID}/snapshots/{_SNAPSHOT_ID}').json
     assert result == {'id': '5ed10b5776f0aa9affa9e3f0', 'datetime': 1575446887339,
                       'available fields': ['feelings', 'user', 'pose', 'depthImage', 'colorImage']}
 
 
-def test_get_field():
-    get_result(42, _SNAPSHOT_ID, 'feelings', _SERVER_HOST, _SERVER_PORT)
-    result = requests.get(
-        f'http://{_SERVER_HOST}:{_SERVER_PORT}/users/{_USERID}/snapshots/{_SNAPSHOT_ID}/feelings').json()
+def test_get_field(api_fixture):
+    result = api_fixture.get(
+        f'/users/{_USERID}/snapshots/{_SNAPSHOT_ID}/feelings').json
     assert result == {'hunger': 0.0, 'thirst': 0.0, 'exhaustion': 0.0, 'happiness': 0.0}
-
-
