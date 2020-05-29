6b4ee0de8d62663eceadaa3905cd2530696f4b22
baraschner <aschner5@gmail.com>
2020-05-29 08:55:16 -0700

    fix server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,13 +1,15 @@
+import json
 from pathlib import Path
 
+from bson import BSON
 from pytest import fixture
 
-from brain.client import upload_sample
 from brain.server import run_server
 
 RESOURCES = Path(__file__).parent / 'resources'
 
 _SAMPLE_PATH = RESOURCES / 'sample.mind.gz'
+_SNAPSHOT_FILE = RESOURCES / 'snapshot.json'
 _SERVER_IP = '127.0.0.1'
 _SERVER_TEST_PORT = 8000
 _EXPECTED_SUPPORTED = ['feelings', 'pose', 'user', 'depthImage', 'colorImage']
@@ -17,6 +19,13 @@ _RESULT = None
 def publish(m):
     global _RESULT
     _RESULT = m
+
+
+@fixture()
+def snapshot():
+    with open(_SNAPSHOT_FILE) as f:
+        data = json.load(f)
+    return BSON.encode(data)
 
 
 @fixture()
@@ -30,6 +39,6 @@ def test_config(server_fixture):
     assert all(k in result['supported_fields'] for k in _EXPECTED_SUPPORTED)
 
 
-def test_snapshot(server_fixture):
-    upload_sample(_SAMPLE_PATH)
+def test_snapshot(server_fixture, snapshot):
+    server_fixture.post('/snapshot', data=snapshot)
     assert 'userId' in _RESULT and 'feelings' in _RESULT
