dccb2dc84d39ff526ad4246f0a34678ef3b76338
Bar Aschner <aschner5@gmail.com>
2020-05-29 16:09:22 +0300

    make api code better

--- brain/api/api.py
+++ brain/api/api.py
@@ -6,24 +6,23 @@ from pymongo import MongoClient
 from brain.utils import flask_tools, consts
 
 
-def run_api_server(host, port, database_url, test_db=None):
+def run_api_server(host, port, database_url=None, db=None):
     """
-    Runs the api server
+    Runs the api server.
 
     :param host: ip to bind
     :param port: port to bind
     :param database_url: url of database from which the api will get the data
-    :return:
+    :param db: db to use
     """
-    app = Flask(__name__)
-    if test_db is not None:
-        db = test_db
-    else:
+    if db is None:
+        assert database_url is not None
         db = MongoClient(database_url)[consts.DB_NAME]
+
+    app = Flask(__name__)
+    CORS(app)
 
     # automatically initializes api from api_resources
     flask_tools.api_driver(Api(app), __file__, (db,))
-
-    CORS(app)
 
     app.run(host=host, port=port)
