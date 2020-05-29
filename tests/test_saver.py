35bb07db08ff904b058342f85d0d1782a13a03d3
Bar Aschner <aschner5@gmail.com>
2020-05-29 15:40:55 +0300

    fix saver test

--- tests/test_saver.py
+++ tests/test_saver.py
@@ -1,27 +1,24 @@
 from pathlib import Path
 
 import mongomock
-from pytest import fixture
-from pymongo import MongoClient
-from brain.saver import Saver
-from brain.utils import consts
+import pytest
 
-_SAMPLE_PATH = Path('tests/resources/parsed_feelings_queue.json').absolute()
-_MONGO_URL = 'mongodb://127.0.0.1:27017'
+RESOURCES = Path(__file__).parent / 'resources'
+_SAMPLE_PATH = RESOURCES / 'parsed_feelings_queue.json'
+_MONGO_URL = 'mongodb://mongodb:27017'
 
-'''
-@fixture(autouse=True)
+
+@pytest.fixture(autouse=True)
 def mock_db():
-    with mongomock.patch(servers=['localhost', 'mongodb']):
+    with mongomock.patch(servers=["localhost", "mongodb"]):
         yield
 
 
 def test_saver():
+    from brain.saver import Saver
+
     saver = Saver(_MONGO_URL)
     with open(_SAMPLE_PATH) as f:
         data = f.read()
     saver.save('feelings', data)
-    client = MongoClient(_MONGO_URL).brain
-    assert 'feelings' in client.snapshots.find_one()
-    
-'''
+    assert 'feelings' in saver.db.snapshots.find_one({})
