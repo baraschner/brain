6ee8979ea88c53ee4a4d8f0bb5ff5705b9ab0a99
baraschner <aschner5@gmail.com>
2020-05-29 06:37:53 -0700

    improve saver tests

--- tests/test_saver.py
+++ tests/test_saver.py
@@ -5,6 +5,8 @@ import pytest
 
 RESOURCES = Path(__file__).parent / 'resources'
 _SAMPLE_PATH = RESOURCES / 'parsed_feelings_queue.json'
+_USER_PATH = RESOURCES / 'parsed_user_queue.json'
+
 _MONGO_URL = 'mongodb://mongodb:27017'
 
 
@@ -14,7 +16,7 @@ def mock_db():
         yield
 
 
-def test_saver():
+def test_regular_field():
     from brain.saver import Saver
 
     saver = Saver(_MONGO_URL)
@@ -22,3 +24,13 @@ def test_saver():
         data = f.read()
     saver.save('feelings', data)
     assert 'feelings' in saver.db.snapshots.find_one({})
+
+
+def test_save_user_data():
+    from brain.saver import Saver
+
+    saver = Saver(_MONGO_URL)
+    with open(_USER_PATH) as f:
+        data = f.read()
+    saver.save('user', data)
+    assert 'username' in saver.db.users.find_one({})
